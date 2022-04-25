7107f06b8279c361e818655b5723b376b9c4de5d
DESKTOP-1U4HJ0I\jorge <jkokeito@gmail.com>
2022-04-25 18:03:02 +0200

    log out problem

--- my-app/src/pages/logout/log-out.jsx
+++ my-app/src/pages/logout/log-out.jsx
@@ -9,6 +9,7 @@ import { useTranslation } from "react-i18next";
 import LandingHeader from "../../components/landing-header/landing-header";
 import './style.css'
 import PageInfo from "../../components/page-info/page-info";
+import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-router-dom";
 
 function LogOut() {
@@ -18,14 +19,14 @@ function LogOut() {
     return (
         <React.Fragment>
             <LandingHeader></LandingHeader>
-            <Container fluid className={`bg-${theme.light} d-flex flex-rown`}>
+            <Container fluid className={`bg-${theme.background} d-flex flex-rown`}>
                 <Container className="m-0 mb-5">
                     <h1>Sesion cerrada correctamente</h1>
                     <h3>{t('landing.title')}</h3>
                     <p>{t('landing.addUser')}</p>
-                    <Link Style="text-decoration: none" to="/">
+                    <Nav.Link Style="text-decoration: none" href="/">
                         <RecentUser></RecentUser>
-                    </Link>
+                    </Nav.Link>
                 </Container>
             </Container>
             <PageInfo></PageInfo>
